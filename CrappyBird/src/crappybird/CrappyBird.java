43f440eeed360a62e8007b415e3dda0195f5ee0b
James Healey <jameshealey1994@gmail.com>
2014-02-06 22:12:50 +0000

    Initial commit.

---
+++ CrappyBird/src/crappybird/CrappyBird.java
@@ -0,0 +1,16 @@
+package crappybird;
+
+/**
+ * Helicopter Clone Clone.
+ *
+ * @author JamesHealey94 <jameshealey1994.gmail.com>
+ */
+public class CrappyBird {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
